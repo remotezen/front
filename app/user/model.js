1bb8ecb3933e18189c52686b749d4e9691d7b8f0
remotezen <remotezen@bell.net>
2016-12-04 09:40:48 -0500

    working on user form component

--- app/user/model.js
+++ app/user/model.js
@@ -1,7 +1,17 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   username: DS.attr('string'),
   password: DS.attr('string'),
   passwordConfirmation: DS.attr('string'),
+  isValidUserName: Ember.computed.notEmpty('username'),
+  isValidPassword: Ember.computed.notEmpty('password'),
+  isPasswordValid: Ember.computed('password', 
+    'passwordConfirmation', () => { 
+      return 'password' === 'passwordConfirmation';   
+    }),
+  isValidStepOne: Ember.computed.and('isValidUsername', 'isValidPassword'),
+  isValid: Ember.computed.and('isValidStepOne', 'isPasswordValid'),
+  isDisabled: Ember.computed.not('isValid')
 });
--- app/users/new/route.js
+++ app/users/new/route.js
@@ -4,6 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.createRecord('user');
   },
+  setupController: function(controller,model){
+    this._super(controller, model);
+    controller.set('title','Enroll yourself!');
+    controller.set('buttonLabel', 'Create');
+    this.render('users/form');
+  },
   actions: {
     saveUser(newUser) {
       newUser.save().then(() => this.transitionTo('secret'));
